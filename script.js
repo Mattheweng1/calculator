61873391893cc2a06ce6f62d6d5d58dc32b69815
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2023-04-28 19:43:37 -0500

    Create basic operator functions

---
+++ script.js
@@ -0,0 +1,15 @@
+// Basic operator functions
+
+const add = (x,y) => x + y;
+
+const subtract = (x,y) => x - y;
+
+const multiply = (x,y) => x * y;
+
+const divide = (x,y) => x / y;
+
+let numStrOne = "";
+let operatorStr = "";
+let numStrTwo = "";
+
+let displayStr = `${numStrOne}${operatorStr}${numStrTwo}`;
