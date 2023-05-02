94d7154ce49cd64787feb1ed309b9497127b66d1
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2023-05-02 16:59:25 -0500

    Add two buttons to calculator and display messages
    Added 'e' and '^' buttons. Display messages below calculator.
    Also: Changed every 'clear' to 'clr'. If numStrOne is 'NaN',
    clear() before entering any other character. Added padding
    to #title.

--- script.js
+++ script.js
@@ -8,16 +8,22 @@ const multiply = (x,y) => x * y;
 
 const divide = (x,y) => x / y;
 
+const exponent = (x,y) => x ** y;
+
 // Create display and function to update it.
 
-let numStrOne = "";
+let numStrOne = "0";
 let operatorStr = "";
 let numStrTwo = "";
-let displayStr = "";
+let displayStr = "0";
 
 function updateDisplay() {
     displayStr = numStrOne + " " + operatorStr + " " + numStrTwo;
     document.getElementById("calcDisplay").textContent = displayStr;
+}
+
+function updateMessage(message = "Time to calculate.") {
+    document.getElementById("message").textContent = message;
 }
 
 // Round a string type number.
@@ -29,6 +35,11 @@ function roundNumStr(numStr) {
 // Function to evaluate expression and assign it to numStrOne.
 
 function operate() {
+    updateMessage();
+    if (numStrOne.endsWith("e") || numStrTwo.endsWith("e")) {
+        updateMessage("A number needs to come after 'e'.");
+        return;
+    }
     switch (operatorStr) {
         case "+":
             numStrOne = add(+numStrOne, +numStrTwo).toString();
@@ -46,19 +57,42 @@ function operate() {
             numStrTwo = "";
             break;
         case "/":
+            if (+numStrTwo === 0) {
+                updateMessage("Maybe I'll be able to divide by zero after I awaken my third eye.");
+            }
             numStrOne = divide(+numStrOne, +numStrTwo).toString();
             operatorStr = "";
             numStrTwo = "";
             break;
+        case "^":
+            numStrOne = exponent(+numStrOne, +numStrTwo).toString();
+            operatorStr = "";
+            numStrTwo = "";
+            break;
     }
-    numStrOne = roundNumStr(numStrOne);
+    numStrOne = roundNumStr((+numStrOne).toString());
     updateDisplay();
+}
+
+// Clr() if numStrOne is NaN.
+
+function clrIfNaN() {
+    if (numStrOne === "NaN") clr();
 }
 
 // If there's already an operator entered, this will add digits to the second number.
 
 function eventEnterNum(numBtn) {
     numBtn.addEventListener('click', () => {
+        clrIfNaN();
+        updateMessage();
+        if (operatorStr === "" && numStrOne.includes("e") && numStrOne.indexOf("e") === numStrOne.length - 2) {
+            updateMessage("Limited to one digit after 'e'.");
+            return;
+        } else if (operatorStr !== "" && numStrTwo.includes("e") && numStrTwo.indexOf("e") === numStrTwo.length - 2) {
+            updateMessage("Limited to one digit after 'e'.");
+            return;
+        }
         if (operatorStr === "") {
             if (numStrOne === "0") {
                 numStrOne = numBtn.textContent;
@@ -80,31 +114,55 @@ function eventEnterNum(numBtn) {
 
 function eventEnterOperator(operatorBtn) {
     operatorBtn.addEventListener('click', () => {
+        clrIfNaN();
+        updateMessage();
+        if (numStrOne.endsWith("e")) {
+            updateMessage("A number needs to come after 'e'.");
+            return;
+        }
         if (+displayStr === 0) {
+            if (operatorBtn.textContent === "-") {
+                numStrOne = "-";
+            } else {
             numStrOne = "0";
             operatorStr = operatorBtn.textContent;
-        } else if (numStrTwo !== "") {
+            }
+        } else if (numStrTwo !== "" && numStrTwo !== "-") {
             operate();
             operatorStr = operatorBtn.textContent;
-        } else {
+        } else if (operatorStr !== "" && operatorBtn.textContent === "-") {
+            if (numStrTwo === "-") {
+                numStrTwo = "";
+            } else {
+                numStrTwo = "-";
+            }
+        } else if (numStrOne !== "-") {
             operatorStr = operatorBtn.textContent;
         }
-    updateDisplay();
+        updateDisplay();
     });
 }
 
 // Only allow one decimal per number.
 
 function enterDecimal() {
+    clrIfNaN();
+    updateMessage();
     if (operatorStr === "") {
-        if (numStrOne.includes(".")) return;
+        if (numStrOne.includes(".") || numStrOne.includes("e")) {
+            updateMessage("You can't input a decimal there.")
+            return;
+        }
         if (numStrOne === "") {
             numStrOne = "0.";
         } else {
             numStrOne += ".";
         }
     } else {
-        if (numStrTwo.includes(".")) return;
+        if (numStrTwo.includes(".") || numStrTwo.includes("e")) {
+            updateMessage("You can't input a decimal there.");
+            return;
+        }
         if (numStrTwo === "") {
             numStrTwo = "0.";
         } else {
@@ -114,9 +172,25 @@ function enterDecimal() {
     updateDisplay();
 }
 
-// Clear all expression variables and the display.
+// Only allow one e per number and only after a number is there already.
 
-function clear() {
+function enterE() {
+    clrIfNaN();
+    updateMessage();
+    if (operatorStr === "" && +numStrOne !== 0 && numStrOne !== "-" && !(numStrOne.includes("e"))) {
+        numStrOne += "e";
+    } else if (operatorStr !== "" && +numStrTwo !== 0 && numStrTwo !== "-" && !(numStrTwo.includes("e"))) {
+        numStrTwo += "e";
+    } else {
+        updateMessage("You can't input 'e' there.");
+    }
+    updateDisplay();
+}
+
+// clr all expression variables and the display.
+
+function clr() {
+    updateMessage();
     numStrOne = "0";
     operatorStr = "";
     numStrTwo = "";
@@ -127,6 +201,7 @@ function clear() {
 // Delete last character entered, from the variable it was entered into.
 
 function del() {
+    updateMessage();
     if (numStrTwo !== "") {
         numStrTwo = numStrTwo.slice(0,-1);
     } else if (operatorStr !== "") {
@@ -148,11 +223,14 @@ operatorBtns.forEach((operatorBtn) => eventEnterOperator(operatorBtn));
 const decimalBtn = document.getElementById("decimal");
 decimalBtn.addEventListener('click', () => enterDecimal());
 
+const eBtn = document.getElementById("e");
+eBtn.addEventListener('click', () => enterE());
+
 const equalsBtn = document.getElementById("equals");
 equalsBtn.addEventListener('click', () => operate());
 
-const clearBtn = document.getElementById("clear");
-clearBtn.addEventListener('click', () => clear());
+const clrBtn = document.getElementById("clr");
+clrBtn.addEventListener('click', () => clr());
 
 const delBtn = document.getElementById("del");
 delBtn.addEventListener('click', () => del());
